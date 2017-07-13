3e01e6d0b26fc8ffc78762a116477ca2d69c09f8
Yassin Mziya <ymm22@cornell.edu>
2017-07-13 10:25:13 +0300

    new repo from existing project

---
+++ client/components/App.jsx
@@ -0,0 +1,15 @@
+/*
+	./client/components/App.jsx
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class App extends React.Component {
+	render() {
+		return (
+			<div style={{textAlign: 'center'}}>
+        		<h1>Hello World</h1>
+      		</div>
+		);
+	}
+}
